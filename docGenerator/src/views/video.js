095a136f6ee68b16a9bcfa14e6a943706383027e
Atanas Trifonov <77271452+tanagaHub@users.noreply.github.com>
2022-06-14 09:42:02 +0300

    Update video.js

--- docGenerator/src/views/video.js
+++ docGenerator/src/views/video.js
@@ -3,6 +3,7 @@ import { html } from 'https://unpkg.com/lit-html?module';
 export const videoTemplate = () => html`
  <p>Към видеото има субтитри, не забравяйте да ги активирате!</p>
  <iframe width="300" height="200" src="https://www.youtube.com/embed/AeKKaEtk3gg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+  
   <p><form method="POST" action="https://www.epay.bg/v3main/paylogin">
 <input type="hidden" name="MIN" value="5873645786">
 <input type="hidden" name="TOTAL" value="5">
